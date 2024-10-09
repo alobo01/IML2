319e8cd8a7d57703eb3368bd0e1868d59ee90daa
brunosangom <brunosangom@gmail.com>
2024-10-09 15:39:25 +0200

    KNN test and voting policies fixed

--- KNN.py
+++ KNN.py
@@ -15,22 +15,33 @@ class KNNAlgorithm:
         """
         Initialize the kNNAlgorithm with:
         - k: Number of neighbors to consider.
-        - distance_metric: A function to calculate the distance between samples ('euclidean_distance', 'manhattan_distance', 'OTHER_distance').
+        - distance_metric: A function to calculate the distance between samples ('euclidean_distance', 'manhattan_distance', 'clark_distance').
         - weighting_method: Weighting method to apply to the distance ('equal_weight', 'OTHER1_weight', 'OTHER2_weight').
-        - voting_policy: Voting technique to determine the class ('majority_class', 'inverse_distance_weighting', 'shepard').
+        - voting_policy: Voting technique to determine the class ('majority_class', 'inverse_distance_weighted', 'shepard').
         """
         self.k = k
         self.distance_metric = distance_metric
         self.weighting_method = weighting_method
         self.voting_policy = voting_policy
+        self.train_data = None
+        self.train_labels = None
 
-    def get_weights(self, vec: pd.Series) -> np.ndarray:
+    def fit(self, train_data: pd.DataFrame, train_labels: pd.Series):
+        """
+        Fit the kNN model with training data and labels.
+        """
+        weights = self.get_weights(train_data)
+
+        self.train_data = train_data.multiply(weights, axis=1)
+        self.train_labels = train_labels
+
+    def get_weights(self, train_data: pd.DataFrame) -> np.ndarray:
         if self.weighting_method == 'equal_weight':
-            return np.ones(vec.size)
+            return np.ones(train_data.shape[1])
         if self.weighting_method == 'OTHER1_weight':
-            return np.ones(vec.size)
+            return np.ones(train_data.shape[1])
         if self.weighting_method == 'OTHER2_weight':
-            return np.ones(vec.size)
+            return np.ones(train_data.shape[1])
         else:
             raise ValueError(f"Unsupported weighting method: {self.weighting_method}")
 
@@ -38,34 +49,39 @@ class KNNAlgorithm:
         """
         Calculate list of distances between vectors of 2 DataFrames.
         """
-        weights = self.get_weights(test_row)
-
         if self.distance_metric == 'euclidean_distance':
-            return [(int(index), self.euclidean_distance(test_row, row, weights)) for index, row in train_data.iterrows()]
-        elif self.voting_policy == 'manhattan_distance':
-            return [(int(index), self.manhattan_distance(test_row, row, weights)) for index, row in train_data.iterrows()]
-        elif self.voting_policy == 'OTHER_distance':
-            return [(int(index), self.OTHER_distance(test_row, row, weights)) for index, row in train_data.iterrows()]
+            return [(int(index), self.euclidean_distance(test_row, row)) for index, row in train_data.iterrows()]
+        elif self.distance_metric == 'manhattan_distance':
+            return [(int(index), self.manhattan_distance(test_row, row)) for index, row in train_data.iterrows()]
+        elif self.distance_metric == 'clark_distance':
+            return [(int(index), self.clark_distance(test_row, row)) for index, row in train_data.iterrows()]
         else:
             raise ValueError(f"Unsupported distance function: {self.distance_metric}")
 
-    def euclidean_distance(self, vec1: Union[pd.Series, np.ndarray], vec2: Union[pd.Series, np.ndarray], weights) -> float:
+    def euclidean_distance(self, vec1: pd.Series, vec2: pd.Series) -> float:
         """
         Euclidean distance metric.
         """
-        return np.sqrt(np.sum(weights * (vec1 - vec2) ** 2))
+        return np.sqrt(np.sum((vec1 - vec2) ** 2))
 
-    def manhattan_distance(self, vec1: Union[pd.Series, np.ndarray], vec2: Union[pd.Series, np.ndarray], weights) -> float:
+    def manhattan_distance(self, vec1: pd.Series, vec2: pd.Series) -> float:
         """
         Manhattan distance metric.
         """
-        return np.sum(weights * np.abs(vec1 - vec2))
+        return np.sum(np.abs(vec1 - vec2))
 
-    def OTHER_distance(self, vec1: Union[pd.Series, np.ndarray], vec2: Union[pd.Series, np.ndarray], weights) -> float:
+    def clark_distance(self, vec1: pd.Series, vec2: pd.Series) -> float:
         """
-        OTHER distance metric.
+        Clark distance metric.
         """
-        return np.sqrt(np.sum(weights * (vec1 - vec2) ** 2))
+        numerator = np.abs(vec1 - vec2)
+        denominator = vec1 + vec2
+        squared_ratio = (numerator / denominator) ** 2
+
+        # Handle divisions by zero (if vec1 + vec2 == 0, the result should be ignored in sum)
+        squared_ratio = squared_ratio.replace([np.inf, -np.inf], np.nan).dropna()
+
+        return np.sqrt(squared_ratio.sum())
 
     def get_neighbors(self, train_data: pd.DataFrame, train_labels: pd.Series, test_row: pd.Series) -> tuple[Any, Any]:
         """
@@ -88,8 +104,8 @@ class KNNAlgorithm:
 
         if self.voting_policy == 'majority_class':
             return self.majority_class_vote(neighbors_labels)
-        elif self.voting_policy == 'inverse_distance_weighting':
-            return self.inverse_distance_weighting(neighbors_data, neighbors_labels, test_row)
+        elif self.voting_policy == 'inverse_distance_weighted':
+            return self.inverse_distance_weighted(neighbors_data, neighbors_labels, test_row)
         elif self.voting_policy == 'shepard':
             return self.shepard_vote(neighbors_data, neighbors_labels, test_row)
         else:
@@ -101,31 +117,44 @@ class KNNAlgorithm:
         """
         return neighbors_labels.mode()[0]  # Get the most frequent label
 
-    def inverse_distance_weighting(self, neighbors_data, neighbors_labels, test_row: pd.Series) -> float:
+    def inverse_distance_weighted(self, neighbors_data, neighbors_labels, test_row: pd.Series) -> float:
         """
         Inverse distance weighting: Weight the class labels by the inverse of their distances.
         """
-        distances = self.get_distances(neighbors_data, test_row)
+        distances = self.get_distances(neighbors_data, test_row) # @todo This is already calculated. Maybe store and bring here
         # Avoid division by zero
         weights = [1 / (d + 1e-5) for _, d in distances]  # Add a small constant to avoid division by zero
-        weighted_labels = neighbors_labels * weights
-        return weighted_labels.sum() / sum(weights)
+        class_vote = {}
+
+        for i, weight in enumerate(weights):
+            label = neighbors_labels.iloc[i]
+            if label not in class_vote:
+                class_vote[label] = 0
+            class_vote[label] += weight
+
+        # Find the label with the highest weighted vote
+        max_vote_label = max(class_vote, key=class_vote.get)
+
+        return max_vote_label
 
     def shepard_vote(self, neighbors_data, neighbors_labels, test_row: pd.Series) -> float:
         """
         Shepard's method: Use a power-based weighting.
         """
         distances = self.get_distances(neighbors_data, test_row)
-        weights = [(1 / (d + 1e-5)) ** 2 for _, d in distances]  # Shepard's weighting (power of 2)
-        weighted_labels = neighbors_labels * weights
-        return weighted_labels.sum() / sum(weights)
+        weights = [np.exp(-d) for _, d in distances]  # Shepard's weighting (power of 2)
+        class_vote = {}
 
-    def fit(self, train_data: pd.DataFrame, train_labels: pd.Series):
-        """
-        Fit the kNN model with training data and labels.
-        """
-        self.train_data = train_data
-        self.train_labels = train_labels
+        for i, weight in enumerate(weights):
+            label = neighbors_labels.iloc[i]
+            if label not in class_vote:
+                class_vote[label] = 0
+            class_vote[label] += weight
+
+        # Find the label with the highest weighted vote
+        max_vote_label = max(class_vote, key=class_vote.get)
+
+        return max_vote_label
 
     def predict(self, test_data: pd.DataFrame) -> List[Union[int, float]]:
         """
