fcbecaa12102cbdda2d3b443b9a7b16c71814d70
Pedro Agundez Fernandez <chernowi@gmail.com>
2024-10-29 22:49:04 +0100

    added preprocessed csvs for mushroom

--- Mushroom/mushroom_dataset_analysis.py
+++ Mushroom/mushroom_dataset_analysis.py
@@ -6,12 +6,6 @@ complete_df = DataPreprocessor.get_whole_dataset_as_df(
     "../datasets/mushroom/mushroom.fold.000000.train.arff"
 )
 
-# test preprocessor
-reader = DataPreprocessor(complete_df)
-preprocessed_df = reader.fit_transform(cat_encoding="label")
-
-reader.save("mushroom_preprocessor.joblib")
-
 analyzer.save_dataframe_description_analysis(complete_df)
 analyzer.save_feature_distributions_by_class(complete_df)
 analyzer.analyze_feature_importance(complete_df)
