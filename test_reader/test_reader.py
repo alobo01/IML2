b6d256c6e74d95ac99e9ad45d1e1137cb0b280f9
Pedro Agundez Fernandez <chernowi@gmail.com>
2024-11-01 10:57:48 +0100

    deleted unused code

--- test_reader/test_reader.py
+++
@@ -1,5 +0,0 @@
-from Reader import DataPreprocessor
-
-reader = DataPreprocessor("test_reader/adult.fold.000000.train.arff")
-pd = reader.fit_transform(config_path="test_reader/adult.fold.config.json")
-print()
