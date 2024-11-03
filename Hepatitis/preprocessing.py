afc06ef11bce93771c93ab83627ce2c8690777b9
brunosangom <brunosangom@gmail.com>
2024-11-03 14:49:51 +0100

    full main.py

--- Hepatitis/preprocessing.py
+++ Hepatitis/preprocessing.py
@@ -1,8 +1,19 @@
+import os.path
 from classes.Reader import DataPreprocessor
+from test_reducing.dyingyang import dataset
+
+if __name__ == "__main__":
+    dataset_path = '..\\Hepatitis'
+else:
+    dataset_path = 'Hepatitis'
+
+original_folds_path = os.path.join(dataset_path, '../datasets/hepatitis')
+path1 = os.path.join(original_folds_path, "hepatitis.fold.000000.train.arff")
+path2 = os.path.join(original_folds_path, "hepatitis.fold.000000.test.arff")
 
 complete_df = DataPreprocessor.get_whole_dataset_as_df(
-    "../datasets/hepatitis/hepatitis.fold.000000.train.arff",
-    "../datasets/hepatitis/hepatitis.fold.000000.test.arff",
+    path1,
+    path2
 )
 
 binary_features  = [
@@ -28,8 +39,8 @@ for i in range(10):
     fold_str = f"{i:06d}"
 
     # File paths for the current fold
-    train_file = f"../datasets/hepatitis/hepatitis.fold.{fold_str}.train.arff"
-    test_file = f"../datasets/hepatitis/hepatitis.fold.{fold_str}.test.arff"
+    train_file = os.path.join(original_folds_path, f"hepatitis.fold.{fold_str}.train.arff")
+    test_file = os.path.join(original_folds_path, f"hepatitis.fold.{fold_str}.test.arff")
 
     # Load as Dataframes and remove columns with missing values
     train_data = DataPreprocessor.load_arff(train_file).drop(columns=removed_features)
@@ -40,12 +51,14 @@ for i in range(10):
     train_data_preprocessed = reader.fit_transform(ordinal_features=binary_features)
 
     # Save the preprocessor for this fold
-    preprocessor_save_path = f"preprocessor_instances/hepatitis.fold.{fold_str}.preprocessor.joblib"
+    preprocessor_save_path = os.path.join(dataset_path, f"preprocessor_instances/hepatitis.fold.{fold_str}.preprocessor.joblib")
     reader.save(preprocessor_save_path)
 
     # Save train fold as csv
-    train_data_preprocessed.to_csv(f"preprocessed_csvs/hepatitis.fold.{fold_str}.train.csv")
+    preprocessed_train_path = os.path.join(dataset_path, f"preprocessed_csvs/hepatitis.fold.{fold_str}.train.csv")
+    train_data_preprocessed.to_csv(preprocessed_train_path)
 
     # Preprocess and save test fold as csv
     test_data_preprocessed = reader.transform(test_data)
-    test_data_preprocessed.to_csv(f"preprocessed_csvs/hepatitis.fold.{fold_str}.test.csv")
+    preprocessed_test_path = os.path.join(dataset_path, f"preprocessed_csvs/hepatitis.fold.{fold_str}.test.csv")
+    test_data_preprocessed.to_csv(preprocessed_test_path)
--- Mushroom/preprocessing.py
+++ Mushroom/preprocessing.py
@@ -1,9 +1,20 @@
+import os.path
 from classes.Reader import DataPreprocessor
+from test_reducing.dyingyang import dataset
+
+if __name__ == "__main__":
+    dataset_path = '..\\Mushroom'
+else:
+    dataset_path = 'Mushroom'
+
+original_folds_path = os.path.join(dataset_path, '../datasets/mushroom')
+path1 = os.path.join(original_folds_path, "mushroom.fold.000000.train.arff")
+path2 = os.path.join(original_folds_path, "mushroom.fold.000000.test.arff")
 
 # Load the complete dataset and identify columns with excessive missing values
 complete_df = DataPreprocessor.get_whole_dataset_as_df(
-    "../datasets/mushroom/mushroom.fold.000000.train.arff",
-    "../datasets/mushroom/mushroom.fold.000000.test.arff",
+    path1,
+    path2
 )
 
 # Binary features are considered ordinal for the preprocesser
@@ -24,8 +35,8 @@ for i in range(10):
     fold_str = f"{i:06d}"
 
     # File paths for the current fold
-    train_file = f"../datasets/mushroom/mushroom.fold.{fold_str}.train.arff"
-    test_file = f"../datasets/mushroom/mushroom.fold.{fold_str}.test.arff"
+    train_file = os.path.join(original_folds_path, f"mushroom.fold.{fold_str}.train.arff")
+    test_file = os.path.join(original_folds_path, f"mushroom.fold.{fold_str}.test.arff")
 
     # Load training and test dataframes and drop columns with high missing values
     train_data = DataPreprocessor.load_arff(train_file).drop(columns=removed_features)
@@ -36,12 +47,14 @@ for i in range(10):
     train_data_preprocessed = reader.fit_transform(ordinal_features=ordinal_features)
 
     # Save the preprocessor for this fold
-    preprocessor_save_path = f"preprocessor_instances/mushroom.fold.{fold_str}.preprocessor.joblib"
+    preprocessor_save_path = os.path.join(dataset_path, f"preprocessor_instances/mushroom.fold.{fold_str}.preprocessor.joblib")
     reader.save(preprocessor_save_path)
 
     # Save train fold as csv
-    train_data_preprocessed.to_csv(f"preprocessed_csvs/mushroom.fold.{fold_str}.train.csv")
+    preprocessed_train_path = os.path.join(dataset_path, f"preprocessed_csvs/mushroom.fold.{fold_str}.train.csv")
+    train_data_preprocessed.to_csv(preprocessed_train_path)
 
     # Preprocess and save test fold as csv
     test_data_preprocessed = reader.transform(test_data)
-    test_data_preprocessed.to_csv(f"preprocessed_csvs/mushroom.fold.{fold_str}.test.csv")
+    preprocessed_test_path = os.path.join(dataset_path, f"preprocessed_csvs/mushroom.fold.{fold_str}.test.csv")
+    test_data_preprocessed.to_csv(preprocessed_test_path)
